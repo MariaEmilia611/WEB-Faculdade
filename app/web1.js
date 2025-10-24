ab50d6c8d60d898d23c43a82fc8e208988dc3afb
Maria <maria945duarteferreira@outlook.com>
2025-10-24 20:36:35 -0300

    Add interatividade

--- app/web1.js
+++ app/web1.js
@@ -1,31 +1,32 @@
-var url = 'http://10.11.10.43/web1/public/produtos'
+var url = 'http://200.144.254.118:8000/produtos'
 
-function mostraDados() {
+$(document).ready(function() {
+    mostraDados()
+})
 
-   $.get(url, function(produtos) {
-      $('#tb_produtos tbody').remove(linha);
-      var linha = ''
-      for(var i = 0; i< produtos.length; i++) {
-         linha = '<tr>'
-         linha += '<td>' + produtos(i).id + '</td>'
-         linha += '<td>' + produtos(i).nome + '</td>'
-         linha += '<td>' + produtos(i).valor + '</td>'
-         linha += '<button class="btn btn-warning btn-sm">Alterar</button>'
-         linha += ''
-         linha += '</tr>'
+function mostraDados(){
+    $.get(url, function(produtos){
+        $('#tb_produtos tbody tr').remove();
 
-         $('#tb_produtos tbody').append(linha);
-      }
+        var linha = ''
+        for(var i = 0; i < produtos.length; i++){
+            linha = '<tr>'
+            linha += '<td>' + produtos[i].id + '</td>'
+            linha += '<td>' + produtos[i].nome + '</td>'
+            linha += '<td>' + produtos[i].valor + '</td>'
+            linha += '<td><button onclick="alterar(' + produtos[i].id + ')" class="btn btn-warning btn-sm">Alterar</button></td>'
+            linha += '<td><button onclick="excluir(' + produtos[i].id + ')" class="btn btn-danger btn-sm">Excluir</button></td>'
+            linha += '</tr>'
 
-   });
-
-
+            $('#tb_produtos tbody').append(linha);
+        }
+    });
 }
 
-function alterar(id) {
-   alert(id)
+function alterar(id){
+    alert('ALTERAR: ' + id)
 }
 
-function excluir(id) {
-   alert(id)
+function excluir(id){
+    alert('EXCLUIR:' + id)
 }
