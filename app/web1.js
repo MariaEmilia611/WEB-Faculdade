14cb34f705d4401222d58789f5c6ae56f9f7e729
Maria <maria945duarteferreira@outlook.com>
2025-11-10 22:09:03 -0300

    Crud:PUT alterar produto

--- app/web1.js
+++ app/web1.js
@@ -1,16 +1,10 @@
 var url = 'http://200.144.254.118:8000/produtos'
 
+var idProduto = 0;
+
 $(document).ready(function() {
     mostraDados()
 })
-
-function novoProduto(){
-    $('#nome').val(''); 
-    $('#valor').val('');
-    $('#dlgProdutos').modal('show'); 
-}
-
-
 
 function mostraDados(){
     $.get(url, function(produtos){
@@ -26,17 +20,83 @@ function mostraDados(){
             linha += '<td><button onclick="excluir(' + produtos[i].id + ')" class="btn btn-danger btn-sm">Excluir</button></td>'            
             linha += '</tr>'
             $('#tb_produtos tbody').append(linha);
-
         }
     });
 }
 
-function adicionar(){
-    alert('ADICIONAR')
+function novoProduto(){
+    idProduto = 0;
+
+    $('#nome').val('');
+    $('#valor').val('');
+    $('#dlgProdutos').modal('show');
+}
+
+function salvar(){
+    if (idProduto == 0){ 
+        criarProduto();
+    } else { 
+        alterarProduto();
+    }
+}
+
+function criarProduto() {
+    var produto = {
+        nome: $('#nome').val(),
+        valor: $('#valor').val()
+    }
+
+    $.ajax({
+        type: "POST",
+        url: url,
+        contentType: 'application/json',
+        data: JSON.stringify(produto),
+        success: function (response) {
+            mostraDados(); 
+            $('#dlgProdutos').modal('hide');
+            $('#msgSucesso').text('Registro Incluído com Sucesso.');
+            $('#dlgSucesso').modal('show');
+        },
+        error: function (erro) {
+            console.log(erro);
+            alert('Ocorreu um erro ao salvar o produto.');
+        }
+    });
 }
 
 function alterar(id){
-    alert('ALTERAR: ' + id)
+    idProduto = id;
+
+    $.get(url + '/' + id, function(produto){
+        $('#nome').val(produto.nome);
+        $('#valor').val(produto.valor);
+        $('#dlgProdutos').modal('show');
+    });
+}
+
+function alterarProduto() {
+    var produto = {
+        id: idProduto,
+        nome: $('#nome').val(),
+        valor: $('#valor').val() 
+    }
+
+    $.ajax({
+        type: "PUT", 
+        url: url + '/' + produto.id, 
+        contentType: 'application/json',
+        data: JSON.stringify(produto),
+        success: function (response) {
+            mostraDados(); 
+            $('#dlgProdutos').modal('hide');
+            $('#msgSucesso').text('Registro Alterado com Sucesso.');
+            $('#dlgSucesso').modal('show'); 
+        },
+        error: function (erro) {
+            console.log(erro);
+            alert('Ocorreu um erro ao alterar o produto.');
+        }
+    });
 }
 
 function excluir(id){
